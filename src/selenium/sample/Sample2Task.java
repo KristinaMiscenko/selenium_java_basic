9e5642851eb747e1c4edb20ff6ed63b975c280b1
*kristinaMiscenko* <*kristinammm7@gmail.com*>
2020-10-27 11:31:39 +0200

    Day1

--- src/selenium/sample/Sample2Task.java
+++ src/selenium/sample/Sample2Task.java
@@ -17,8 +17,8 @@ public class Sample2Task {
     @Before
     public void startingTests() throws Exception {
         // from Sample 1:
-        String libWithDriversLocation = System.getProperty("user.dir") + "\\lib\\";
-        System.setProperty("webdriver.chrome.driver", libWithDriversLocation + "chromedriver.exe");
+        String libWithDriversLocation = System.getProperty("user.dir") + "/lib/";
+        System.setProperty("webdriver.chrome.driver", libWithDriversLocation + "chromedriver");
         // declaration above:
         driver = new ChromeDriver();
 
@@ -36,18 +36,22 @@ public class Sample2Task {
     public void findElementByID() throws Exception {
 //         TODO:
 //         get text "Heading 2 text" using id
+        System.out.println(driver.findElement(By.id("heading_2")).getText());
     }
 
     @Test
     public void findElementByName() throws Exception {
 //         TODO:
 //         get attribute "id" and "value" of button "This is also a button" using name
+        System.out.println(driver.findElement(By.name("randomButton2")).getAttribute("value"));
+        System.out.println(driver.findElement(By.name("randomButton2")).getAttribute("id"));
     }
 
     @Test
     public void findElementByClassFirst() throws Exception {
 //         TODO:
 //         get first text of class "test" (should be "Test Text 1")
+        System.out.println(driver.findElement(By.className("text")).getText()); // "sample text 1"
     }
 
     @Test
@@ -56,5 +60,21 @@ public class Sample2Task {
 //         get size text of class "test" (should be 5)
 //         get text of class "test"
 //         get third text of class "test" (should be "Test Text 4")
+        System.out.println(driver.findElements(By.id("headingasdga")).size());
+        System.out.println(driver.findElements(By.className("test1")).size());
+        List<WebElement> allElementsWithClass = driver.findElements(By.className("test"));
+//
+        for (WebElement elementWithClass : allElementsWithClass) {
+            System.out.println(elementWithClass.getText());
+            //            sample text 1
+            //            sample text 2
+            //            unbelievable sample text
+            //            amazing sample text
+            //            dummy text
+        }
+        System.out.println("-----------------------");
+        System.out.println(driver.findElements(By.className("text")).get(0).getText());
+        System.out.println(driver.findElements(By.className("text")).get(2).getText());
     }
-}
+    }
+
--- src/selenium/sample/Sample3Task.java
+++ src/selenium/sample/Sample3Task.java
@@ -19,8 +19,8 @@ public class Sample3Task {
     @Before
     public void startingTests() throws Exception {
         // from Sample 1:
-        String libWithDriversLocation = System.getProperty("user.dir") + "\\lib\\";
-        System.setProperty("webdriver.chrome.driver", libWithDriversLocation + "chromedriver.exe");
+        String libWithDriversLocation = System.getProperty("user.dir") + "/lib/";
+        System.setProperty("webdriver.chrome.driver", libWithDriversLocation + "chromedriver");
         // declaration above:
         driver = new ChromeDriver();
 
@@ -39,6 +39,10 @@ public class Sample3Task {
 //         TODO:
 //         check how many element with class "test" there are on page (5)
 //         check that value of second button is "This is also a button"
+
+        int expectedNumberOfElements = 5;
+        int actualNumberOfElements = driver.findElements(By.className("randomButton2")).size();
+        assertEquals(expectedNumberOfElements, actualNumberOfElements);
     }
 
     @Test
@@ -47,12 +51,29 @@ public class Sample3Task {
 //         check that it is True that value of second button is
 //         "this is Also a Button" if you ignore Caps Locks
 //         fail with custom error message:
+        String elementTextOnPage = driver.findElement(By.cssSelector("randomButton2")).getText();
+        assertTrue(elementTextOnPage.equals("This is also a button"));
+//        assertTrue("Expecting the element with class 'unbelievable' to have text 'ajshdka'", elementTextOnPage.contains("ajshdka"));
+        assertTrue(elementTextOnPage.equalsIgnoreCase("This is ALso a buTTon"));
+        // pass:
+        assertTrue(true);
+        // fail:
+//        assertTrue(false);
+
     }
 
     @Test
     public void assertFalseTask() throws Exception {
 //         TODO:
 //        check that it is False that value of second button is "This is a button"
+        String elementTextOnPage = driver.findElement(By.cssSelector("randomButton2")).getText();
+        assertFalse(!elementTextOnPage.equals("This is also a button"));
+        assertFalse(elementTextOnPage.equals("wrong text"));
+        assertFalse(elementTextOnPage.contains("wrong text"));
+        // fail:
+//        assertFalse(true);
+        // pass:
+        assertFalse(false);
     }
 
     @Test
@@ -60,5 +81,13 @@ public class Sample3Task {
 //        TODO:
 //        check that none of items with class "test"
 //        contain number 190
+        String elementTextOnPage = driver.findElement(By.cssSelector("test")).getText();
+        assertFalse(!elementTextOnPage.equals("190"));
+        assertFalse(elementTextOnPage.equals("wrong text"));
+        assertFalse(elementTextOnPage.contains("wrong text"));
+        // fail:
+//        assertFalse(true);
+        // pass:
+        assertFalse(false);
     }
 }
